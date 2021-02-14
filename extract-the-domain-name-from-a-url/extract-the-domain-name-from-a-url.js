0dee978d92277951613a23ad4b52e12ac9b2ff18
absoluteRango <31278752+absoluteRango@users.noreply.github.com>
2021-02-14 19:53:36 +0100

    extract the domain name from a url

---
+++ extract-the-domain-name-from-a-url/extract-the-domain-name-from-a-url.js
@@ -0,0 +1,9 @@
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string.
+
+function domainName(url){
+    return url
+        .replace('https://', '')
+        .replace('http://', '')
+        .replace('www.', '')
+        .split('.')[0]
+}
